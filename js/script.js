c131d345c21e3eb1279364e090d86409efd831dd
manikandan <manimani19508@gmail.com>
2022-10-11 16:49:11 +0530

    add new file

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//datatypes
+
+
+//1.Boolean type//
+//2.Null type//
+//3.Undefined type//
+//4.Number type//
+//5.BigInt type//
+//6.String type//
+//7.Symbol type//
+
+
+
+
+var a=10;
+var b=25;
+console.log(a+b);
+//string
+/*var d = 10
+var c = "string"
+console.log(d+c);*/
+//boolean
+/*console.log(a>b)*/
+//nan
+/*console.log(d/c);*/
+//null
+/*var f = ""
+console.log(f);*/
+//undefined
+/*var country = 
+console.log(country);*/
+//array
+/*var cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);*/
+//object
+/*var person = {firstName:"virat", lastName:"kholi", age:32, state:"india"};
+console.log(person);*/
+
+
+
+
+
+
+
